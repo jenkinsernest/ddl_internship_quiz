e3601f134a702cfd93ccdbb947091681551d448e
jenkinsernest <legumchim2@gmail.com>
2020-11-25 13:39:39 +0100

    Create problem_two.java

---
+++ problem_two.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+String value="";
+
+for(int a=0; a<n; a++){
+    value+="#";
+          System.out.println(value);
+
+}
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
